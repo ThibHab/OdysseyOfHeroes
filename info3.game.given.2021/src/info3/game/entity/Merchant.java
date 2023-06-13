00ca9cccf54163502de29cc429d21ec482f3de68
Yann MLEKO <yann.mlekoy@gmail.com>
2023-06-13 17:04:37 +0200

    Created NPC class with Villager and Merchant subclasses

---
+++ info3.game.given.2021/src/info3/game/entity/Merchant.java
@@ -0,0 +1,7 @@
+package info3.game.entity;
+
+public class Merchant extends NPC {
+	public Merchant() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ info3.game.given.2021/src/info3/game/entity/Villager.java
@@ -0,0 +1,7 @@
+package info3.game.entity;
+
+public class Villager extends NPC {
+	public Villager() {
+		// TODO Auto-generated constructor stub
+	}
+}
