04402fc4d2d82b8f3b403087d785adc4aee31688
Guillaume Bellone <guillaume.bellone02@gmail.com>
2023-06-13 16:21:01 +0200

    Maps
    Added 4 extends class of Map. Three of them are the three different map that are used in the game. The DebugMap is used for debugging the project.
    TODO: Need to implement the maps

---
+++ info3.game.given.2021/src/info3/game/map/WorldMap.java
@@ -0,0 +1,12 @@
+package info3.game.map;
+
+import info3.game.entity.Entity;
+
+public class WorldMap extends Map {
+
+	public WorldMap(int nb_x, int nb_y, Entity p1, Entity p2) {
+		super(nb_x, nb_y, p1, p2);
+		// TODO Auto-generated constructor stub
+	}
+
+}
