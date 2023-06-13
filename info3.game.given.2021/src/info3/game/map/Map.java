b2fd7a6bad28f1605c22a75a6ad70b352919aa62
Guillaume Bellone <guillaume.bellone02@gmail.com>
2023-06-13 16:13:18 +0200

    Abstract Class Map
    - Implemented constructor and the methode to generate Entity at random location in an area
    - Changed interface IMap

--- info3.game.given.2021/src/info3/game/map/Map.java
+++ info3.game.given.2021/src/info3/game/map/Map.java
@@ -1,18 +1,84 @@
 package info3.game.map;
 
 import java.awt.Graphics;
+import java.util.Random;
 
-public class Map implements IMap {
-	public Tile[][] matrix;
-	
-	public Map() {
-		// TODO Auto-generated constructor stub
+import info3.game.entity.Entity;
+import info3.game.entity.Location;
+
+public abstract class Map implements IMap {
+	public Tile[][] map;
+	int lenX, lenY;
+	Entity player1, player2;
+
+	public Map(int nb_x, int nb_y, Entity p1, Entity p2) {
+		this.lenX = nb_x;
+		this.lenY = nb_y;
+		this.player1 = p1;
+		this.player2 = p2;
 	}
 
-	@Override
-	public void paint(Graphics g) {
-		// TODO Auto-generated method stub
+	public void setEntityRandomly(int x, int y, int areaSize, int spaceBetween, Entity ent, long seed) {
+		if (!(ent instanceof Bush) && !(ent instanceof Rock) && !(ent instanceof Tree)) {
+			return;
+		}
+		Random r = new Random(seed);
+		for (int i = x; i < x + areaSize; i++) {
+			for (int j = y; j < y + areaSize; j++) {
+				int n = r.nextInt(5);
+				if (n == 1) {
+					boolean already = false;
+					int rec_x = i - spaceBetween;
+					int rec_y = j - spaceBetween;
+					while (rec_x < 0) {
+						while (rec_y < 0) {
+							if (map[lenX + rec_x][lenY + rec_y].entity != null) {
 
+								already = true;
+							}
+							System.out.println("Carré: (" + rec_x + ";" + rec_y + ")");
+							rec_y++;
+						}
+						while (rec_y <= j + spaceBetween) {
+							if (map[lenX + rec_x][rec_y].entity != null) {
+								already = true;
+							}
+							System.out.println("Carré: (" + rec_x + ";" + rec_y + ")");
+							rec_y++;
+						}
+						rec_x++;
+					}
+					while (rec_x <= i + spaceBetween) {
+						rec_y = j - spaceBetween;
+						while (rec_y < 0) {
+							if (map[rec_x][lenY + rec_y].entity != null) {
+								already = true;
+							}
+							System.out.println("Carré: (" + rec_x + ";" + rec_y + ")");
+							rec_y++;
+						}
+						while (rec_y <= j + spaceBetween) {
+							if (map[rec_x][rec_y].entity != null) {
+								already = true;
+							}
+							System.out.println("Carré: (" + rec_x + ";" + rec_y + ")");
+							rec_y++;
+						}
+						rec_x++;
+					}
+					if (!already) {
+						if (ent instanceof Bush) {
+							map[i][j].entity = new Bush();
+						} else if (ent instanceof Rock) {
+							map[i][j].entity = new Rock();
+						} else {
+							map[i][j].entity = new Tree();
+						}
+
+						System.out.println("Objet créé en: (" + i + ";" + j + ")");
+					}
+				}
+			}
+		}
 	}
-
 }
