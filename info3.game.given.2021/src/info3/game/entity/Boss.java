559553fa2c30a900cd47957f8187a53bc5769358
Yann MLEKO <yann.mlekoy@gmail.com>
2023-06-20 16:53:57 +0200

    Changed boss' offensive methods
    Hit is the new method for mob spawning, Pop for fireball launch attack and Wizz for the last one which is not implemented yet

--- info3.game.given.2021/src/info3/game/entity/Boss.java
+++ info3.game.given.2021/src/info3/game/entity/Boss.java
@@ -6,9 +6,12 @@ import java.util.concurrent.ThreadLocalRandom;
 import info3.game.automata.*;
 import info3.game.constants.EntitiesConst;
 import info3.game.constants.ImagesConst;
+import info3.game.map.Tile;
 
 public class Boss extends Mob {
 	public int phase;
+	public int mapHeight;
+	public int mapWidth;
 	
 	public Boss(Location l) {
 		super();
@@ -18,9 +21,12 @@ public class Boss extends Mob {
 		this.weaponDamage = EntitiesConst.BOSS_BASE_DAMAGE;
 		this.weaponRange = EntitiesConst.BOSS_RANGE;
 		this.speed = EntitiesConst.BOSS_SPEED;
-		// --- TODO manage automaton ---
-		this.automaton = null;
-		this.currentState = null;
+
+		for (Aut_Automaton next : EntitiesConst.GAME.listAutomata) {
+			if (next.name.equals(name))
+				this.automaton = next;
+		}
+		this.currentState = automaton.initial;
 		// -----------------------------
 		this.category = Aut_Category.A;
 		// --- TODO manage sprite properly ---
@@ -28,6 +34,9 @@ public class Boss extends Mob {
 		this.imageIndex = 0;
 		// -----------------------------------
 		this.phase = 0;
+		
+		this.mapHeight = EntitiesConst.MAP.lenY;
+		this.mapWidth = EntitiesConst.MAP.lenX;
 	}
 
 	@Override
@@ -36,184 +45,76 @@ public class Boss extends Mob {
 			d = this.direction;
 		}
 		
-		Random random = new Random();
-		int randomAttack = 0;
-		if (this.phase == 0) {
-			randomAttack = random.nextInt(2);
-			if (randomAttack == 0) {
-				this.fireballAttack(d);
-			} else {
-				this.spawnMobs();
-			}
-		} else {
-			randomAttack = random.nextInt(3);
-			switch (randomAttack) {
-			case 0:
-				this.fireballAttack(d);
-				break;
-			case 1:
-				this.spawnMobs();
-				break;
-			case 2:
-				this.flameBreathAttack(d);
-				break;
-			default:
-				break;
-			}
-		}
-	}
-	
-	public void spawnMobs() {
 		int bossPosX = (int) this.location.getX();
-		int bossPosY = (int) this.location.getY();
 		Random random = new Random();
 		for (int i = 0; i < EntitiesConst.BOSS_MOB_SPAWN_NUMBER; i++) {
-			float randomPosX = ThreadLocalRandom.current().nextInt(bossPosX - EntitiesConst.BOSS_MOB_SPAWN_RANGE, bossPosX + EntitiesConst.BOSS_MOB_SPAWN_RANGE);
-			float randomPosY = ThreadLocalRandom.current().nextInt(bossPosY - EntitiesConst.BOSS_MOB_SPAWN_RANGE, bossPosY + EntitiesConst.BOSS_MOB_SPAWN_RANGE);
+			float randomPosX = random.nextInt(bossPosX);
+			float randomPosY = random.nextInt(this.mapHeight);
 			Location mobLocation = new Location(randomPosX, randomPosY);
-			
-			int randomMob = random.nextInt(2);
-			if (randomMob == 0) {
-				new Skeleton(mobLocation);
-			} else {
-				new Goblin(mobLocation);
+			while(!this.checkEntitiesAround(mobLocation)) {
+				randomPosX = random.nextInt(bossPosX);
+				randomPosY = random.nextInt(this.mapHeight);
+				mobLocation = new Location(randomPosX, randomPosY);
 			}
-		}
-	}
-	
-	public void fireballAttack(Aut_Direction d) {
-		int mapHeight = EntitiesConst.MAP.lenY;
-		int mapWidth = EntitiesConst.MAP.lenX;
-		int dragonTopPosHeight = mapHeight / 2 + 1;
-		int dragonSubPosHeight = mapHeight / 2 - 2;
-		int dragonTopPosWidth = mapWidth / 2 + 1;
-		int dragonSubPosWidth = mapWidth / 2 - 2;
-		Random random = new Random();
-		switch (d) {
-		case N:
-			int randomNumberOfFireballs = random.nextInt(EntitiesConst.BOSS_FIREBALL_NUMBER_MAX);
-			int[] eliminatedPos = new int[randomNumberOfFireballs];
-			for (int i = 0; i < randomNumberOfFireballs; i++) {
-				int randomPos = random.nextInt(mapWidth);
-				boolean isPosAlreadyUsed = false;
-				for (int j = 0; j < eliminatedPos.length; j++) {
-					if (randomPos == eliminatedPos[j]) {
-						isPosAlreadyUsed = true;
-					}
-				}
-				
-				if (!isPosAlreadyUsed) {
-					// TODO add direction and location in constructor
-					new Projectile(this);
-					eliminatedPos[i] = randomPos;
-				}
-			}
-			break;
-		case S:
-			
-			break;
-		case W:
-			
-			break;
-		case E:
-			
-			break;
-		default:
-			break;
-		}
-	}
-	
-	public void flameBreathAttack(Aut_Direction d) {
-		// TODO position calculation is not good
-		int mapHeight = EntitiesConst.MAP.lenY;
-		int mapWidth = EntitiesConst.MAP.lenX;
-		int mapHalfHeight = 0, mapHalfWidth = 0;
-		switch (d) {
-		case N:
-			for (int row = 0; row < mapHeight / 2 - 2; row++) {
-				for (int column = row; column < mapWidth - row; column++) {
-					Entity entity = EntitiesConst.MAP_MATRIX[row][column].entity;
-					if (entity != null) {
-						entity.health -= EntitiesConst.BOSS_FLAME_ATTACK_DAMAGE;
-					}
-					// TODO animation for the flame breath
-				}
-			}
-			break;
-		case S:
-			if (mapHeight % 2 != 0) {
-				mapHalfHeight = mapHeight / 2 + 1;
-			} else {
-				mapHalfHeight = mapHeight / 2;
-			}
-			
-			for (int row = mapHeight; row > mapHalfHeight + 1; row--) {
-				int shiftIndex = mapHeight - row;
-				for (int column = shiftIndex; column < mapWidth - shiftIndex; column++) {
-					Entity entity = EntitiesConst.MAP_MATRIX[row][column].entity;
-					if (entity != null) {
-						entity.health -= EntitiesConst.BOSS_FLAME_ATTACK_DAMAGE;
-					}
-					// TODO animation for the flame breath
-				}
-			}
-			break;
-		case W:
-			for (int column = 0; column < mapWidth / 2 - 2; column++) {
-				for (int row = column; row < mapHeight - column; row++) {
-					Entity entity = EntitiesConst.MAP_MATRIX[row][column].entity;
-					if (entity != null) {
-						entity.health -= EntitiesConst.BOSS_FLAME_ATTACK_DAMAGE;
-					}
-					// TODO animation for the flame breath
-				}
-			}
-			break;
-		case E:
-			if (mapWidth % 2 != 0) {
-				mapHalfWidth = mapWidth / 2 + 1;
-			} else {
-				mapHalfWidth = mapWidth / 2;
-			}
-			
-			for (int column = mapWidth; column > mapHalfWidth + 1; column--) {
-				int shiftIndex = mapWidth - column;
-				for (int row = shiftIndex; row < mapWidth - shiftIndex; row++) {
-					Entity entity = EntitiesConst.MAP_MATRIX[column][row].entity;
-					if (entity != null) {
-						entity.health -= EntitiesConst.BOSS_FLAME_ATTACK_DAMAGE;
-					}
-					// TODO animation for the flame breath
-				}
-			}
-			break;
-		default:
-			break;
-		}
-	}
 
+			boolean randomMob = random.nextBoolean();
+			Tile tile = EntitiesConst.MAP_MATRIX[(int) mobLocation.getX()][(int) mobLocation.getY()];
+			if (randomMob) {
+				tile.entity = new Skeleton(mobLocation);
+			} else {
+				tile.entity = new Goblin(mobLocation);
+			}
+		}
+	}
+	
+	public boolean checkEntitiesAround(Location location) {
+		int locationXPos = (int) location.getX();
+		int locationYPos = (int) location.getY();
+		
+		if (EntitiesConst.MAP_MATRIX[locationXPos][locationYPos].entity != null) {
+			return false;
+		}
+		
+		for (int row = locationXPos - EntitiesConst.BOSS_MOB_SPAWN_RANGE; row < locationXPos + EntitiesConst.BOSS_MOB_SPAWN_RANGE; row++) {
+			for (int column = locationYPos - EntitiesConst.BOSS_MOB_SPAWN_RANGE; column < locationYPos + EntitiesConst.BOSS_MOB_SPAWN_RANGE; column++) {
+				Entity entity = EntitiesConst.MAP_MATRIX[row][column].entity;
+				if (entity != null && entity.category == Aut_Category.AT) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
 	@Override
-	public void Pop(Aut_Direction d, Aut_Category c) {
-		// TODO Auto-generated method stub
-		super.Pop(d, c);
+	public void Pop(Aut_Direction d) {
+		Random random = new Random();
+		int randomPosY = random.nextInt(mapHeight);
+		while (randomPosY == 0 || randomPosY == this.mapHeight) {
+			randomPosY = random.nextInt(mapHeight);
+		}
+		
+		EntitiesConst.MAP_MATRIX[(int) this.location.getX()][randomPosY - 1].entity = new Projectile(this, d);
+		EntitiesConst.MAP_MATRIX[(int) this.location.getX()][randomPosY].entity = new Projectile(this, d);
+		EntitiesConst.MAP_MATRIX[(int) this.location.getX()][randomPosY + 1].entity = new Projectile(this, d);
 	}
 
 	@Override
 	public void Wizz(Aut_Direction d, Aut_Category c) {
-		// TODO Auto-generated method stub
-		super.Wizz(d, c);
+//		boolean endReached = false;
+//		int attackPosX = (int) this.location.getX() - 1;
+//		while (!endReached) {
+//			for (int column = attackPosX; column < EntitiesConst.BOSS_FLAME_ATTACK_SIZE; column--) {
+//				for (int row = 0; row < this.mapHeight; row++) {
+//					
+//				}
+//			}
+//		}
 	}
 
 	@Override
 	public void Power() {
-		// TODO Auto-generated method stub
-		super.Power();
+		this.phase++;
 	}
-
-	@Override
-	public void Throw(Aut_Direction d, Aut_Category category) {
-		// TODO Auto-generated method stub
-		super.Throw(d, category);
-	}
-
 }
