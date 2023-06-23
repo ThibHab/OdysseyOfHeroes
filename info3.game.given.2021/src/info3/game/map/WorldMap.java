77744c4b77bda2ec4742f02e9297707a9f4adc7f
Yann MLEKO <yann.mlekoy@gmail.com>
2023-06-23 10:31:19 +0200

    Added generation of entrances in the WorldMap andd player placement
    Managed players placement when returning to the WorldMap. Also added map swap management

--- info3.game.given.2021/src/info3/game/map/WorldMap.java
+++ info3.game.given.2021/src/info3/game/map/WorldMap.java
@@ -1,5 +1,6 @@
 package info3.game.map;
 
+import info3.game.constants.EntitiesConst;
 import info3.game.entity.*;
 
 public class WorldMap extends Map {
@@ -11,14 +12,26 @@ public class WorldMap extends Map {
 		this.setLac(9, 12, 3);
 		this.setVillage(30, 30, 25);
 		this.setForest(70, 70, 30, 2);
-			
 		
 		this.setEntityRandomly(0, 0, lenX - 2, 2, "Tree", 2, 30);
 		this.setEntityRandomly(0, 0, lenX - 2, 2, "Bush", 2, 10);
 		this.setEntityRandomly(0, 0, lenX - 2, 2, "Rock", 2, 15);
 		
-		setPlayer(29, 31, p1);
-		setPlayer(31, 31, p2);
+		this.setDungeonEntrance(EntitiesConst.DUNGEON_ENTRANCE_X_POS, EntitiesConst.DUNGEON_ENTRANCE_Y_POS);
+		this.setMazeEntrance(EntitiesConst.MAZE_ENTRANCE_X_POS, EntitiesConst.MAZE_ENTRANCE_Y_POS);
+		
+		if (EntitiesConst.MAP == null) {
+			setPlayer(29, 31, p1);
+			setPlayer(31, 31, p2);
+		} else if (EntitiesConst.MAP instanceof MazeMap) {
+			setPlayer(EntitiesConst.MAZE_ENTRANCE_X_POS, EntitiesConst.MAZE_ENTRANCE_Y_POS + 1, p1);
+			setPlayer(EntitiesConst.MAZE_ENTRANCE_X_POS + 1, EntitiesConst.MAZE_ENTRANCE_Y_POS + 1, p2);
+		} else if (EntitiesConst.MAP instanceof DungeonMap) {
+			setPlayer(EntitiesConst.DUNGEON_ENTRANCE_X_POS, EntitiesConst.DUNGEON_ENTRANCE_Y_POS + 1, p1);
+			setPlayer(EntitiesConst.DUNGEON_ENTRANCE_X_POS + 1, EntitiesConst.DUNGEON_ENTRANCE_Y_POS + 1, p2);
+		}
+		
+		EntitiesConst.MAP = this;
+		EntitiesConst.MAP_MATRIX = this.map;
 	}
-
 }
